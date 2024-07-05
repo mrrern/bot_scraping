d68e50eae73baf891e31322a85cdee36b1ed92fe
Richard Brito <britorichard5@gmail.com>
2024-07-05 19:53:26 -0400

    test new

--- logic.py
+++ logic.py
@@ -6,33 +6,34 @@ from selenium.webdriver.common.keys import Keys
 from tkinter import messagebox
 from selenium.webdriver.support import expected_conditions as EC
 
-# Función para verificar si la fecha está dentro de dos meses
 def is_within_two_months(date_str):
     from datetime import datetime, timedelta
-    date_format = "%d/%m/%Y"  # Ajustar el formato de fecha según sea necesario
+    date_format = "%d/%m/%Y"  # Adjust the date format as needed
     date = datetime.strptime(date_str, date_format)
     return date <= datetime.today() + timedelta(days=60)
 
-def login_and_book_test(favorite_centre):
-    # Configuración del navegador
-    driver = webdriver.Safari()
+def login_and_book_test():
+    # Configure Safari browser
+    options = webdriver.SafariOptions()
+    options.page_load_strategy = 'normal'  # Load the entire page before proceeding
+    driver = webdriver.Safari(options=options)
     driver.maximize_window()
 
-    # Navegar a la página inicial
+    # Navigate to the homepage
     driver.get("https://www.gov.uk/book-pupil-driving-test")
 
-    # Esperar a que la página cargue completamente la página inicial
-    wait = WebDriverWait(driver, 200)
-    current_utl =  driver.current_url
-
+    # Wait for the homepage to fully load
+    wait = WebDriverWait(driver, 20)
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'testcentre')))
 
-    # Indicar al usuario que ingrese sus credenciales manualmente
+    # Inform the user to manually enter their credentials
     messagebox.showinfo(title="SIIIII", message="Ahora si entra.")
 
-    # Esperar a que la URL cambie, lo que indica que el usuario ha iniciado sesión
+    # Wait for the URL to change, indicating successful login
+    current_url = driver.current_url
     initial_url = driver.current_url
     while driver.current_url == initial_url:
         time.sleep(6)
 
-    driver.close()
+    # Close the Safari browser
+    driver.quit()
